bf6f371b9526a7bc919bf8e0aa42bcd0d0d14049
Truong Nguyen Vu <nguyenvutruong191@gmail.com>
2022-01-07 14:59:07 +0700

    upload code

---
+++ data.js
@@ -0,0 +1,78 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-242f8e65-67dc-4c36-bdbd-c1000774a598-color-20481",
+      "name": "242f8e65-67dc-4c36-bdbd-c1000774a598-color-2048[1]",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -1.7955745426982581,
+        "pitch": 0.0881116386497034,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1506749619604193,
+          "pitch": 0.15376144238341816,
+          "rotation": 6.283185307179586,
+          "target": "1-01ec2137-73de-4c98-8dbb-1c1d13b30dc9-color-20481"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-01ec2137-73de-4c98-8dbb-1c1d13b30dc9-color-20481",
+      "name": "01ec2137-73de-4c98-8dbb-1c1d13b30dc9-color-2048[1]",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0094484530302683,
+          "pitch": 0.41983973392998664,
+          "rotation": 0,
+          "target": "0-242f8e65-67dc-4c36-bdbd-c1000774a598-color-20481"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.594662033925605,
+          "pitch": -0.15813461346279922,
+          "title": "Title",
+          "text": "Text1232131"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
